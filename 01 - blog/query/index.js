9c7a25207dc7dd2fa4ef3ff2a11c6f9b93b86d13
SirHarveyBix <79848679+SirHarveyBix@users.noreply.github.com>
2023-01-29 11:38:55 +0100

    add query service: display content towards event bus

---
+++ 01 - blog/query/index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const PORT = 4002;
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+const posts = {};
+
+app.get('/posts', (request, response) => {
+  response.send(posts);
+});
+
+app.post('/events', (request, response) => {
+  const { type, data } = request.body;
+
+  if (type === 'PostCreated') {
+    const { id, title } = data;
+    posts[id] = { id, title, comments: [] };
+  }
+
+  if (type === 'CommentCreated') {
+    const { id, content, postId } = data;
+    const post = posts[postId];
+    post.comments.push({ id, content });
+  }
+
+  console.info('posting :\n', posts);
+  response.send({});
+});
+
+app.listen(PORT, () => {
+  console.info(`Listening on ${PORT}`);
+});
