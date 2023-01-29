e2730326e2f764e5f5d0e82af68414589c1136ce
SirHarveyBix <79848679+SirHarveyBix@users.noreply.github.com>
2023-01-29 11:16:17 +0100

    creat bus event management

--- 01 - blog/comments/index.js
+++ 01 - blog/comments/index.js
@@ -1,35 +1,49 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { randomBytes } = require('crypto');
-const cors = require('cors')
-
-const PORT = 4001
+const cors = require('cors');
+const axios = require('axios');
+const PORT = 4001;
 
 const app = express();
 app.use(bodyParser.json());
-app.use(cors())
+app.use(cors());
 
-const commentByPostId = {}
+const commentByPostId = {};
 
 app.get('/posts/:id/comments', (request, response) => {
-
-  response.send(commentByPostId[request.params.id] || [])
+  response.send(commentByPostId[request.params.id] || []);
 });
 
-app.post('/posts/:id/comments', (request, response) => {
+app.post('/posts/:id/comments', async (request, response) => {
   const commentId = randomBytes(4).toString('hex');
-  const { content } = request.body
-  const comments = commentByPostId[request.params.id] || []
+  const { content } = request.body;
+  const comments = commentByPostId[request.params.id] || [];
 
   comments.push({
     id: commentId,
     content,
-  })
-  commentByPostId[request.params.id] = comments
+  });
+  commentByPostId[request.params.id] = comments;
 
-  response.status(201).send(comments)
+  await axios
+    .post('http://localhost:4005/events', {
+      type: 'CommentCreated',
+      data: {
+        id: commentId,
+        content,
+        postId: request.params.id,
+      },
+    })
+    .catch((error) => console.error(error.message));
+
+  response.status(201).send(comments);
 });
 
+app.post('/events', (request, response) => {
+  console.info('Received Event : ', request.body.type);
+  response.send({});
+});
 
 app.listen(PORT, () => {
   console.info(`Listening on ${PORT}`);
--- 01 - blog/posts/index.js
+++ 01 - blog/posts/index.js
@@ -1,28 +1,44 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { randomBytes } = require('crypto');
-const cors = require('cors')
+const cors = require('cors');
+const axios = require('axios');
 
-const PORT = 4000
+const PORT = 4000;
 
 const app = express();
 app.use(bodyParser.json());
-app.use(cors())
+app.use(cors());
 
 const posts = {};
 
 app.get('/posts', (request, response) => response.send(posts));
 
-app.post('/posts', (request, response) => {
+app.post('/posts', async (request, response) => {
   const id = randomBytes(4).toString('hex');
   const { title } = request.body;
 
   posts[id] = {
     id,
-    title
+    title,
   };
 
+  await axios
+    .post('http://localhost:4005/events', {
+      type: 'PostCreated',
+      data: {
+        id,
+        title,
+      },
+    })
+    .catch((error) => console.error(error.message));
+
   response.status(201).send(posts[id]);
+});
+
+app.post('/events', (request, response) => {
+  console.info('Received Event : ', request.body.type);
+  response.send({});
 });
 
 app.listen(PORT, () => {
