7b72a4a051109759151a41057024ace67891f767
SirHarveyBix <79848679+SirHarveyBix@users.noreply.github.com>
2023-02-26 16:38:28 +0100

    transformation de currentuser en middleware

--- 02.ticketing/auth/src/routes/current-user.ts
+++ 02.ticketing/auth/src/routes/current-user.ts
@@ -1,19 +1,15 @@
 import express, { Request, Response } from 'express';
-import jwt from 'jsonwebtoken';
+
+import { currentUser } from '../middleware/current-user';
 
 const router = express.Router();
 
-router.get('/api/users/currentuser', (request: Request, response: Response) => {
-  if (!request.session?.jwt) {
-    return response.send({ currentUser: null });
+router.get(
+  '/api/users/currentuser',
+  currentUser,
+  (request: Request, response: Response) => {
+    response.send({ currentUser: request.currentUser || null });
   }
-
-  try {
-    const payload = jwt.verify(request.session.jwt, process.env.JWT_KEY!); // already checked
-    response.send({ currentUser: payload });
-  } catch (error) {
-    response.send({ currentUser: null });
-  }
-});
+);
 
 export { router as currentUserRouter };
