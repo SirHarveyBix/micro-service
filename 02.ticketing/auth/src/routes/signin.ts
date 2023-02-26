fd071b7f4c8f7d5720a28ca28288bb418f3dcd78
SirHarveyBix <79848679+SirHarveyBix@users.noreply.github.com>
2023-02-26 15:42:11 +0100

    creation de la route signin

--- 02.ticketing/auth/src/routes/signin.ts
+++ 02.ticketing/auth/src/routes/signin.ts
@@ -1,9 +1,53 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { body } from 'express-validator';
+import jwt from 'jsonwebtoken';
+
+import { validateRequest } from '../middleware/validate-request';
+import { User } from '../models/user';
+import { BadRequestError } from '../errors/bad-request.error';
+import { PasswordManager } from '../services/password-manager';
 
 const router = express.Router();
 
-router.post('/api/users/signin', (request, response) => {
-  response.send('signin!');
-});
+router.post(
+  '/api/users/signin',
+  [
+    body('email') //
+      .isEmail()
+      .withMessage('Email must be valid.'),
+
+    body('password')
+      .trim()
+      .notEmpty()
+      .withMessage('You must provide a password'),
+  ],
+  validateRequest,
+  async (request: Request, response: Response) => {
+    const { email, password } = request.body;
+
+    const existingUser = await User.findOne({ email });
+    if (!existingUser) {
+      throw new BadRequestError('Invalid Credientials');
+    }
+
+    const passwordsMatch = await PasswordManager.compare(
+      existingUser.password,
+      password
+    );
+    if (!passwordsMatch) {
+      throw new BadRequestError('Invalid Credientials');
+    }
+
+    // generate JWT
+    const userJwt = jwt.sign(
+      { id: existingUser.id, email: existingUser.email },
+      process.env.JWT_KEY! // already checked
+    );
+    //store on sesion object
+    request.session = { jwt: userJwt };
+
+    response.status(200).send(existingUser);
+  }
+);
 
 export { router as signinRouter };
