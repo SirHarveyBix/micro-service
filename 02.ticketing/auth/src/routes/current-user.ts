f56dded8ce575723644fdb47c1d34a6b76620ed5
SirHarveyBix <79848679+SirHarveyBix@users.noreply.github.com>
2023-03-05 16:31:38 +0100

    fix: rempacement de middleware & errors par custom npm module

--- 02.ticketing/auth/src/routes/current-user.ts
+++ 02.ticketing/auth/src/routes/current-user.ts
@@ -1,6 +1,6 @@
 import express, { Request, Response } from 'express';
 
-import { currentUser } from '../middleware/current-user';
+import { currentUser } from '@sh-ticket/common';
 
 const router = express.Router();
 
