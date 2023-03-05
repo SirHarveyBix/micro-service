b6e1f5e2459f354b39ae0ba7b5fb084b82e94931
SirHarveyBix <79848679+SirHarveyBix@users.noreply.github.com>
2023-03-05 17:22:35 +0100

    edit MONGO_URI : auth

--- 02.ticketing/auth/src/index.ts
+++ 02.ticketing/auth/src/index.ts
@@ -8,10 +8,13 @@ const start = async () => {
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined');
   }
+  if (!process.env.MONGO_URI) {
+    throw new Error('MONGO_URI must be defined');
+  }
 
   try {
     mongoose.set('strictQuery', true);
-    await mongoose.connect('mongodb://auth-mongo-service:27017/auth');
+    await mongoose.connect(process.env.MONGO_URI);
     console.log('Connected to Mongo.');
   } catch (error) {
     console.error(error);
